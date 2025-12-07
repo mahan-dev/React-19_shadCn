08beafa53d3da276bf5f38a0c916948a3266ba34
mahan.dev <mahansoodbakhsh@yahoo.com>
2025-12-07 22:18:45 +0330

    intialize Shadcn

---
+++ core/components/layout/Layout.tsx
@@ -0,0 +1,15 @@
+"use client"
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import { AppSidebar } from "@/components/AppAside";
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <SidebarProvider>
+      <AppSidebar />
+      <main>
+        <SidebarTrigger />
+        {children}
+      </main>
+    </SidebarProvider>
+  );
+}
